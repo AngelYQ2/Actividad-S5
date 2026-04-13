a856d40cc94e7cc8af5107dbcea35f9580ffc509
jpbenavente <jpbenaventerojas@gmail.com>
2026-04-13 18:40:43 -0500

    Client Controller

--- obj/Debug/net9.0/ActividadS5.AssemblyInfo.cs
+++ obj/Debug/net9.0/ActividadS5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActividadS5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac66102cdd00c610dca22a2773724724eaa6c56")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActividadS5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActividadS5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
