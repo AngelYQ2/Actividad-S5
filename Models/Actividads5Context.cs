b243df629eae7592e250abeddb1c82ee644b7d79
jpbenavente <jpbenaventerojas@gmail.com>
2026-04-14 00:07:50 -0500

    Update BD

--- Models/Actividads5Context.cs
+++ Models/Actividads5Context.cs
@@ -33,11 +33,7 @@ public partial class Actividads5Context : DbContext
     public virtual DbSet<Proyectoempleado> Proyectoempleados { get; set; }
 
     public virtual DbSet<Tarea> Tareas { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=actividads5;user=root;password=;", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.42-mysql"));
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
